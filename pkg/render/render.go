b21566a251a0081da5a3c65703ea9a63bd4eac59
rach4you <42122680+rach4you@users.noreply.github.com>
2022-12-28 23:14:33 +0100

    Update render.go

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,10 +8,25 @@ import (
 
 // RenderTemplate renders templates using html / template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("../../templates/" + tmpl)
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing template:", err)
 		return
+	}
+}
+
+// RenderTemplate renders templates using html / template
+/*
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+
+	t, err := template.ParseFiles("../../templates/" + tmpl)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = t.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
